d80531a021b85153181266f03d25406ca9350ebb
tom avilius <tomavilius@tomavilius.in>
2025-06-10 12:30:20 +0530

    Start first response.

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
-from app.api.routes import router
 
-app = FastAPI(title="Fake News Gateway API")
-app.include_router(router)
+app = FastAPI()
 
+
+@app.get("/result")
+async def hello():
+    return {
+        "msg": "Hello there!"
+    }
--- app/services/scraper.py
+++
@@ -1,8 +0,0 @@
-import httpx
-
-async def get_articles(title: str) -> list:
-    async with httpx.AsyncClient() as client:
-        response = await client.post("http://localhost:8001/scrape", json={"query": title})
-        response.raise_for_status()
-        return response.json()["articles"]
-
