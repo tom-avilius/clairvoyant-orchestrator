3adb8594c74d9c30bfbf10055a4c949856046c0e
tom avilius <tomavilius@tomavilius.in>
2025-06-20 20:42:42 +0530

    Document main.py|

--- app/main.py
+++ app/main.py
@@ -1,10 +1,16 @@
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
+
+# to handle the result (NLI pipelining) endpoint
 from api import result
+# to handle the sources (trustworthy) enpoint
 from api import sources
 
+# instance of fast api
 app = FastAPI()
 
+# to allow the frontend to talk
+# NOTE: This is used for development only.
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000"],
@@ -13,5 +19,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# inlcude the /analyze route
 app.include_router(result.router)
+# include the /sources route
 app.include_router(sources.router)
